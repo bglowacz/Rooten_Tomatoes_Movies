b3fa8057fe09801df1a23d91ffab0b4ba5cb3e91
bglowacz <153766784+bglowacz@users.noreply.github.com>
2024-01-25 17:02:31 +0100

    Update Top_10_days_when_movies_was_released.sql

--- Sql queries/Top_10_days_when_movies_was_released.sql
+++ Sql queries/Top_10_days_when_movies_was_released.sql
@@ -1,4 +1,4 @@
-INSERT INTO `select 	
+select 	
 		Release_Date 		as day
 		,max(amount_of_movies) as amount_of_movies_per_one_day
 from 	(select
@@ -9,14 +9,4 @@ from 	(select
 		group by 1,2) rtm
 group by 1
 order by 2 desc 
-limit 10` (Release_Date,amount_of_movies_per_one_day) VALUES
-	 ('2002-01-01',28),
-	 ('2000-01-01',25),
-	 ('2003-01-01',25),
-	 ('2001-01-01',24),
-	 ('2018-09-14',23),
-	 ('1988-01-01',23),
-	 ('1993-01-01',22),
-	 ('2016-10-21',21),
-	 ('1992-01-01',21),
-	 ('2018-10-26',20);
+limit 10
