608a3b743a8c9b708a9f8f4012f335a7315af585
bglowacz <153766784+bglowacz@users.noreply.github.com>
2024-01-25 17:09:47 +0100

    Update Max_amount_of_movies_released_in_one_year.sql

--- Sql queries/Max_amount_of_movies_released_in_one_year.sql
+++ Sql queries/Max_amount_of_movies_released_in_one_year.sql
@@ -1,6 +1,6 @@
 select 	
-		date_format(Release_Date, "%Y") as year_of_released
-		, count(Release_Date) as amount_of_movies
+	date_format(Release_Date, "%Y") as year_of_released
+	, count(Release_Date) as amount_of_movies
 from rooten_tomatoes_movies rtm 
 group by 1
 order by 2 desc 
