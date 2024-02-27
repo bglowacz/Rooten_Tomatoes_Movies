2a9e4564fe53a8a04a651226b4047e1754282687
bglowacz <153766784+bglowacz@users.noreply.github.com>
2024-02-27 11:03:07 +0100

    Update Amount_of_movies_released_in_one_month.sql

--- Sql queries/Amount_of_movies_released_in_one_month.sql
+++ Sql queries/Amount_of_movies_released_in_one_month.sql
@@ -1,5 +1,5 @@
 select 
-	date_format(Release_Date, "%M") as month_of_released
+	date_format(Release_Date, "%M") as month_of_release
 	, count(Release_Date) as amount_of_movies
 from rooten_tomatoes_movies rtm 
 group by 1
